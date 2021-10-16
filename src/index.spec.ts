6d2dab8914583eafcbd25c49b99caddba32a8649
Mathieu Eveillard <mathieu.eveillard@gmail.com>
2021-10-16 15:03:52 +0200

    Gist: lenses

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,7 +1,94 @@
-import { systemUnderTest } from ".";
+import { Lens, composeLenses, Reducer, composeReducerWithLens } from ".";
 
-describe("Test of systemUnderTest()", function() {
-  it("Should pass", function() {
-    expect(systemUnderTest()).toEqual(true);
+interface Speed {
+  value: number;
+  unit: "km/h";
+}
+
+interface Car {
+  speed: Speed;
+  seats: number;
+}
+
+const focusOnSpeed: Lens<Car, Speed> = {
+  get: ({ speed }) => speed,
+  set: (car, speed) => ({ ...car, speed }),
+};
+
+const car: Car = {
+  speed: {
+    value: 50,
+    unit: "km/h",
+  },
+  seats: 5,
+};
+
+describe("A lens allows to focus on a specific part of a larger object", () => {
+  test("Read", () => {
+    expect(focusOnSpeed.get(car)).toEqual({
+      value: 50,
+      unit: "km/h",
+    });
+  });
+
+  test("Write", () => {
+    const actual = focusOnSpeed.set(car, {
+      value: 60,
+      unit: "km/h",
+    });
+    const expected: Car = {
+      speed: {
+        value: 60,
+        unit: "km/h",
+      },
+      seats: 5,
+    };
+    expect(actual).toEqual(expected);
+  });
+});
+
+const focusOnValue: Lens<Speed, number> = {
+  get: ({ value }) => value,
+  set: (speed, value) => ({ ...speed, value }),
+};
+
+const focusOnSpeedValue: Lens<Car, number> = composeLenses(focusOnSpeed, focusOnValue);
+
+describe("Lenses compose easily", () => {
+  test("Read", () => {
+    expect(focusOnSpeedValue.get(car)).toEqual(50);
+  });
+
+  test("Write", () => {
+    const actual = focusOnSpeedValue.set(car, 60);
+    const expected: Car = {
+      speed: {
+        value: 60,
+        unit: "km/h",
+      },
+      seats: 5,
+    };
+    expect(actual).toEqual(expected);
+  });
+});
+
+const raiseSpeed: Reducer<Speed> = ({ value }) => ({
+  value: value + 10,
+  unit: "km/h",
+});
+
+const accelerate: Reducer<Car> = composeReducerWithLens(focusOnSpeed, raiseSpeed);
+
+describe("Composition with reducers", function () {
+  test("Composition with reducers", () => {
+    const actual = accelerate(car);
+    const expected: Car = {
+      speed: {
+        value: 60,
+        unit: "km/h",
+      },
+      seats: 5,
+    };
+    expect(actual).toEqual(expected);
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,27 @@
-export function systemUnderTest(): boolean {
-  return true;
+interface Getter<V, U> {
+  (v: V): U;
+}
+
+interface Setter<V, U> {
+  (v: V, u: U): V;
+}
+
+export interface Lens<V, U> {
+  get: Getter<V, U>;
+  set: Setter<V, U>;
+}
+
+export function composeLenses<W, V, U>(first: Lens<W, V>, second: Lens<V, U>): Lens<W, U> {
+  return {
+    get: (w) => second.get(first.get(w)),
+    set: (w, u) => first.set(w, second.set(first.get(w), u)),
+  };
+}
+
+export interface Reducer<State> {
+  (state: State): State;
+}
+
+export function composeReducerWithLens<V, U>({ get, set }: Lens<V, U>, reducer: Reducer<U>): Reducer<V> {
+  return (v: V) => set(v, reducer(get(v)));
 }
